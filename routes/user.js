0a9f9f8b65b3d1f5825119e052a89f147e2402d5
Mayank Raj <mayankrj1224@gmail.com>
2023-02-26 10:29:15 +0530

    api bug fixed

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const userRouter = express.Router();
 const { userlogin, usersignup } = require('../controller/user')
 const asyncHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
-userRouter.post('/login', asyncHandler(userlogin) )
+userRouter.post('/login', asyncHandler(userlogin))
 userRouter.post("/signup", asyncHandler(usersignup));
 
 module.exports = userRouter;
